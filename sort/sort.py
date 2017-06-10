352ea36d6d84527f0b954fbc87f90989eceeeb6e
Thierry Nowak <thinow@gmail.com>
2017-06-10 19:55:53 +0200

    implement sort selection

--- sort/sort.py
+++ sort/sort.py
@@ -6,6 +6,21 @@ def insertion(array):
 
         while position > 0 and array[position - 1] > value:
             array[position] = array[position - 1]
-            position = position - 1
+            position -= 1
 
         array[position] = value
+
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+
+
+def selection(array):
+    for index in range(0, len(array)):
+
+        index_min = index
+        for index_potential_min in range(index + 1, len(array)):
+            if array[index_potential_min] < array[index_min]:
+                index_min = index_potential_min
+
+        swap(array, index, index_min)
--- sort/test.py
+++ sort/test.py
@@ -3,6 +3,9 @@ from copy import copy
 
 from sort import sort
 
+INPUT = [49, 32, 74, 79, 43, 31, 81, 64, 8, 71]
+OUTPUT = [8, 31, 32, 43, 49, 64, 71, 74, 79, 81]
+
 
 class Generator:
     def __init__(self, assertions):
@@ -21,30 +24,8 @@ class TestSort(unittest.TestCase):
     def setUp(self):
         self.generator = Generator(self)
 
-    def test_empty(self):
-        self.generator.try_with(
-            algorithm=sort.insertion,
-            input=[],
-            expected=[]
-        )
-
-    def test_single_value(self):
-        self.generator.try_with(
-            algorithm=sort.insertion,
-            input=[1],
-            expected=[1]
-        )
-
-    def test_two_values(self):
-        self.generator.try_with(
-            algorithm=sort.insertion,
-            input=[1, 2],
-            expected=[1, 2]
-        )
-
     def test_insertion(self):
-        self.generator.try_with(
-            algorithm=sort.insertion,
-            input=[49, 32, 74, 79, 43, 31, 81, 64, 8, 71],
-            expected=[8, 31, 32, 43, 49, 64, 71, 74, 79, 81]
-        )
+        self.generator.try_with(sort.insertion, INPUT, OUTPUT)
+
+    def test_selection(self):
+        self.generator.try_with(sort.selection, INPUT, OUTPUT)
